6c22102cb7ca4ce2f99c5250482a93ad45f3a5ef
briana <109278550+notbriana@users.noreply.github.com>
2025-07-08 21:50:00 +0300

    Create App.cpp

---
+++ Semester 2/DSA/Labs/PriorityQueue/App.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "PriorityQueue.h"
+#include "ExtendedTest.h"
+#include "ShortTest.h"
+
+using namespace std;
+
+
+
+
+int main() {
+	testAll();
+	testAllExtended();
+	testMerge();
+	cout << "End" << endl;
+	system("pause");
+
+}
